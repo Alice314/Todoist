54aebbb781d3444bd1bc91c9cbbb226c69068b80
Alice314 <1257972435@qq.com>
2016-02-24 21:18:28 +0800

    begain data

---
+++ FirstItemActivity.java
@@ -0,0 +1,59 @@
+package com.wusui.todoist;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by fg on 2016/2/13.
+ */
+public class FirstItemActivity extends MainActivity {
+
+
+    public void initToolbar() {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        myToolbar.setTitle("收件箱");
+        myToolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(myToolbar);
+//Set a {@link android.widget.Toolbar Toolbar} to act as the {@link android.support.v7.app.ActionBar} for this Activity window.
+
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,myToolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view_left);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_shoujianxiang:
+                Intent intent = new Intent(FirstItemActivity.this,FirstItemActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_jin:
+                Intent intent1 = new Intent(FirstItemActivity.this,MainActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.nav_seven:
+                Intent intent2 = new Intent(FirstItemActivity.this,ThirdItemActivity.class);
+                startActivity(intent2);
+
+        }
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ MyDatabaseHelper.java
@@ -0,0 +1,39 @@
+package com.wusui.todoist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by fg on 2016/2/12.
+ */
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_USER = "create table user ("
+            +"id integer primary key autoincrement,"
+            +"content text,"
+            +"date text,"
+            +"priority text,"
+            +"deadline text )";
+
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_USER);
+
+        Toast.makeText(mContext,"添加成功",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+                db.execSQL("drop table if exists User");
+                onCreate(db);
+    }
+}
